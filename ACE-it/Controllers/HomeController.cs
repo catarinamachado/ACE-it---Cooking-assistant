a2890300d6c60ee8460ddd7f5c1ade70c4b56c0b
João Vilaça <machadovilaca@gmail.com>
2019-04-06 22:37:30 +0100

    ASP.NET Core (#1)

---
+++ ACE-it/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using ACE_it.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACE_it.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
