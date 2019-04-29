5b16b56c1c959d79eb3cb40e79a57cfda2b81366
Mendess2526 <pedro.mendes.26@gmail.com>
2019-04-29 10:38:02 +0100

    Add create and show

---
+++ ACE-it/Controllers/SessionRecipeController.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ACE_it.Data;
+using ACE_it.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACE_it.Controllers
+{
+    public class SessionRecipeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SessionRecipeController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = (await _userManager.GetUserAsync(HttpContext.User)).Id;
+            var user = await _context.AppUsers.FindAsync(userId);
+            return View(GetCurrentRecipes(user));
+        }
+
+        public async Task<IActionResult> Create(Recipe recipe)
+        {
+            var userId = (await _userManager.GetUserAsync(HttpContext.User)).Id;
+            var user = await _context.AppUsers.FindAsync(userId);
+            var newRecipe = new SessionRecipe(recipe);
+            var session = _context.Sessions.FindAsync(user);
+            session.Result.SessionRecipes.Add(newRecipe);
+            return RedirectToAction("Show");
+        }
+
+        public async Task<IActionResult> Show(int id)
+        {
+            var sessionRecipe = await _context.Sessions.FindAsync(HttpContext.User);
+            return View(sessionRecipe.SessionRecipes.Find(a => a.Id == id));
+        }
+
+        private async Task<List<Recipe>> GetCurrentRecipes(
+            User user)
+        {
+            return (await _context.Sessions.FindAsync(new {user}))
+                .SessionRecipes.ConvertAll(s => s.Recipe);
+        }
+    }
+}
--- ACE-it/Models/SessionRecipe.cs
+++ ACE-it/Models/SessionRecipe.cs
@@ -4,11 +4,18 @@ namespace ACE_it.Models
 {
     public class SessionRecipe
     {
+        public SessionRecipe(Recipe recipe)
+        {
+            Recipe = recipe;
+            Index = 0;
+            Order = 0;
+        }
+
         public int Id { get; set; }
 
         [Required] public Recipe Recipe { get; set; }
 
-        public int Index { get; set; }
-        public int Order { get; set; }
+        private int Index { get; set; }
+        private int Order { get; set; }
     }
 }
