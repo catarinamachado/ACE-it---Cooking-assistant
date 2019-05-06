727eba28fc21968f83f8a3275ef83e890edd9c54
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-06 09:32:39 +0100

    wip

--- ACE-it/Controllers/SessionRecipeController.cs
+++ ACE-it/Controllers/SessionRecipeController.cs
@@ -27,24 +27,34 @@ namespace ACE_it.Controllers
         public async Task<IActionResult> Create(int recipe)
         {
             var user = _context.AppUsers.First(r => r.Email == User.Identity.Name);
-            var recipeObj = await _context.Recipes.FindAsync(recipe);
-            var session = _context.Sessions.FirstOrDefault(r => r.User == user);
+            var recipeObj = await _context.Recipes.FirstOrDefaultAsync(r => r.Id == recipe);
+            var session = await _context.Sessions.Include(r => r.SessionRecipes)
+                .FirstOrDefaultAsync(r => r.User == user);
             if (session == null)
             {
                 session = new Session {User = user, SessionRecipes = new List<SessionRecipe>(1)};
                 _context.Sessions.Add(session);
             }
-            var recipeSession = session.SessionRecipes.FirstOrDefault(r => r.Recipe == recipeObj) ??
-                                new SessionRecipe {Recipe = recipeObj};
+
+            var sessionRecipe = session.SessionRecipes.FirstOrDefault(r => r.Recipe == recipeObj);
+            if (sessionRecipe == null)
+            {
+                sessionRecipe = new SessionRecipe {Recipe = recipeObj};
+                session.SessionRecipes.Add(sessionRecipe);
+            }
+
+            _context.SaveChanges();
             return RedirectToAction("Show", "SessionRecipe",
-                new {sessionId = session.Id, recipeSessionId = recipeSession.Id});
+                new {sessionId = session.Id, recipeSessionId = sessionRecipe.Id});
         }
 
         public async Task<IActionResult> Show(int sessionId, int recipeSessionId)
         {
-            await _context.Sessions.ForEachAsync(r => Console.WriteLine(r.ToString()));
-            var session = await _context.Sessions.FirstAsync(s => s.Id == sessionId);
-            return View(session.SessionRecipes.First(a => a.Id == recipeSessionId));
+            var sessionRecipe = (await _context.Sessions
+                    .Include(s => s.SessionRecipes.Select(x => x.Recipe))
+                    .FirstAsync(s => s.Id == sessionId))
+                .SessionRecipes.First(a => a.Id == recipeSessionId);
+            return View(sessionRecipe);
         }
 
         private async Task<List<Recipe>> GetCurrentRecipes(
