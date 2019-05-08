63cc147def0d1df57bc6730661847112e40a52cc
Catarina Machado <catarinamachado11@gmail.com>
2019-05-08 16:58:25 +0100

    Comments begin

--- ACE-it/Controllers/RateController.cs
+++ ACE-it/Controllers/RateController.cs
@@ -19,9 +19,10 @@ namespace ACE_it.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(int? id)
+        public async Task<IActionResult> Index(int? id, bool? commentSent)
         {
             if (id == null) return NotFound();
+            if (commentSent == null) commentSent = false;
 
             var recipe = await _context.Recipes
                 .Include(r => r.UserCompletedRecipes)
@@ -33,7 +34,7 @@ namespace ACE_it.Controllers
                 .First(r => r.Email == User.Identity.Name);
             
             return View(new RateViewModel(
-                user, recipe));
+                user, recipe, (bool) commentSent));
         }
 
         public async Task<IActionResult> React(int id, String userId, String reaction)
@@ -61,23 +62,49 @@ namespace ACE_it.Controllers
             
             var oldUserReaction = _context.UserReactedToRecipes.Find(userId, id);
             
+            //if user wants to remove the reaction
             if (oldUserReaction != null && (oldUserReaction.Reaction == userReactedToRecipe.Reaction))
             {
                 _context.Remove(oldUserReaction);
             }
+            //if user wants to update an old reaction to a new one
             else if(oldUserReaction != null)
             {
                 _context.Remove(oldUserReaction);
                 _context.Add(userReactedToRecipe);
             }
+            //if user wants to just add a new reaction
             else
             {
                 _context.Add(userReactedToRecipe);
             }
 
-            _context.SaveChanges();   
-            
+            _context.SaveChanges();
+
             return RedirectToAction("Index", "Rate", new { id });
+        }
+
+        
+        //MISSING ID COMPLETED RECIPE
+        public async Task<IActionResult> Comment(int id, String userId, String commentary)
+        {
+            var idCompleted = 1;
+
+            var userCompletedRecipe = _context.UserCompletedRecipes.Find(idCompleted);
+            
+            var comment = new Comment
+            {
+                UserCompletedRecipe = userCompletedRecipe, Text = commentary
+            };
+            
+           // _context.UserCompletedRecipes.Find(idCompleted).Comments.Add(comment);
+            
+            
+            
+            //falta aparecer algum pop up a dizer que o comentário foi adicionado
+            //caixa com o comentário ficar vazia depois de aparecer o pop up
+            
+            return RedirectToAction("Index", "Rate", new { Id = id, CommentSent = true });
         }
     }
 }
--- ACE-it/Helper/RateViewModel.cs
+++ ACE-it/Helper/RateViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using ACE_it.Models;
 
 namespace ACE_it.Helper
@@ -6,13 +7,16 @@ namespace ACE_it.Helper
     {
         public User User { get; set; }
         public Recipe Recipe { get; set; }
-        
+        public bool CommentSent { get; set; }
+
         public RateViewModel(
             User user,
-            Recipe recipe)
+            Recipe recipe,
+            bool commentSent)
         {
             User = user;
             Recipe = recipe;
+            CommentSent = commentSent;
         }
     }
 }
