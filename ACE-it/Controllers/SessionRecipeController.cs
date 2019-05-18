6b3edb094cc78f1f1ac56fdd9bc4f3d315077a4c
Mendess2526 <pedro.mendes.26@gmail.com>
2019-05-18 13:22:05 +0100

    Remove unused imports

--- ACE-it/Controllers/SessionRecipeController.cs
+++ ACE-it/Controllers/SessionRecipeController.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using ACE_it.Data;
 using ACE_it.Helper;
