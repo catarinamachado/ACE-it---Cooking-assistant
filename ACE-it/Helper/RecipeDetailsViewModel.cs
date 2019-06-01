c078e9b06ce6251305b9f72f72a2fb9f0490cf16
Catarina Machado <catarinamachado11@gmail.com>
2019-06-01 14:19:16 +0100

    Show public comments (#17)

--- ACE-it/Helper/RecipeDetailsViewModel.cs
+++ ACE-it/Helper/RecipeDetailsViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ACE_it.Models;
 
 namespace ACE_it.Helper
@@ -6,11 +7,13 @@ namespace ACE_it.Helper
     {
         public Recipe Recipe { get; }
         public int? SessionId { get; }
+        public List<Comment> Comments { get; set; }
 
-        public RecipeDetailsViewModel(Recipe recipe, int? sessionId)
+        public RecipeDetailsViewModel(Recipe recipe, int? sessionId, List<Comment> comments)
         {
             Recipe = recipe;
             SessionId = sessionId;
+            Comments = comments;
         }
     }
 }
