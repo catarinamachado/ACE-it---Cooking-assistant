f3eae84e22a83b80b0b7d0d085f004d0af6c8e2f
Catarina Machado <catarinamachado11@gmail.com>
2019-06-02 01:09:33 +0100

    Show private difficulties (#19)

--- ACE-it/Helper/RecipeDetailsViewModel.cs
+++ ACE-it/Helper/RecipeDetailsViewModel.cs
@@ -7,13 +7,17 @@ namespace ACE_it.Helper
     {
         public Recipe Recipe { get; }
         public int? SessionId { get; }
-        public List<Comment> Comments { get; set; }
+        public List<Comment> Comments { get; }
+        public string UserId { get; }
+        public List<string> Difficulties { get; }
 
-        public RecipeDetailsViewModel(Recipe recipe, int? sessionId, List<Comment> comments)
+        public RecipeDetailsViewModel(Recipe recipe, int? sessionId, List<Comment> comments, string userId, List<string> difficulties)
         {
             Recipe = recipe;
             SessionId = sessionId;
             Comments = comments;
+            UserId = userId;
+            Difficulties = difficulties;
         }
     }
 }
